aca46f33b8ee633db3be6b08d86cd180e79061d2
Kerr Hamilton <kerr9928@gmail.com>
2024-07-28 19:34:58 +0000

    Create function called display_slot_machine

--- game.py
+++ game.py
@@ -55,3 +55,14 @@ def spin_slot_machine(rows, cols, symbols):
 
     return columns
 
+def display_slot_machine(columns):
+
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        
+        
+        print()
