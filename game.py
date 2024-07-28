4f61ca194584e72f9de72b5f7664dcceee3c0ef8
Kerr Hamilton <kerr9928@gmail.com>
2024-07-28 18:02:28 +0000

    Create global variables for functions

---
+++ game.py
@@ -0,0 +1,22 @@
+import random
+
+max_lines = 3
+max_bet = 100
+min_bet = 1
+
+rows = 3
+cols = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
