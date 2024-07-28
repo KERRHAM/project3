ec78bee76857753d13101605edadd6774e83e4e3
Kerr Hamilton <kerr9928@gmail.com>
2024-07-28 18:16:59 +0000

    Create Function called check_winnings

--- game.py
+++ game.py
@@ -20,3 +20,20 @@ symbol_value = {
     "C": 3,
     "D": 2
 }
+
+def check_winnings(columns, bets, values, lines):
+    winnings = 0
+    winning_lines = []
+    for lines in range(lines):
+        symbol = columns[0][line]
+        for coloumns in coloumn:
+            symbol_to_check = coloumn[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+        
+            return winnings, winning_lines
+
+
